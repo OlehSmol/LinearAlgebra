ad02ee06a4e37b5a4b3cea2f4cc686c14db3acb2
OlehSmol <smolkin@ucu.edu.ua>
2016-11-09 02:48:26 +0200

    changes

--- matrix.py
+++ matrix.py
@@ -6,41 +6,49 @@ class Matrix():
         self.m = len(matrix)
         self.n = len(matrix[0])
 
-    def parseMatrix(self, matrix):
+    def parse_matrix(self, matrix):
         pass
 
     def display(self):
-        self.displayMatrix(self.matrix)
+        Matrix.display_matrix(self.matrix)
 
-    def displayMatrix(self, matrix):
+    def display_matrix(matrix):
         for row in range(len(matrix)):
             for column in range(len(matrix[0])):
                 print(str(matrix[row][column]), end=" ")
             print()
         print("\n")
 
-    def stringMatrix(self):
+    def string_matrix(self):
         pass
 
-    def UFactorization(self):
+    def u_factorization(self):
         upper = self.matrix
         for pivot in range(self.m - 1):
+            pivot_row = pivot
+            while not upper[pivot_row][pivot] and pivot_row < self.m - 1:
+                pivot_row += 1
+            if not upper[pivot_row][pivot]:
+                continue
+            upper[pivot], upper[pivot_row] = upper[pivot_row], upper[pivot]
             divide_pivot = self.matrix[pivot]/upper[pivot][pivot]
             for row in range(pivot + 1, self.m):
                 if(upper[pivot][pivot] != 0):
                     upper[row] -= divide_pivot*upper[row][pivot]
+
+
         return upper
 
     def determinant(self):
-        upper = self.UFactorization()
+        upper = self.u_factorization()
         det = 1
         for i in range(self.m):
             det *= upper[i][i]
 
         return det
 
-    def isConsistent(self):
-        matrix = self.UFactorization()
+    def is_consistent(self):
+        matrix = self.u_factorization()
         is_consistent = True
         for row in range(self.m):
             is_zero = True
